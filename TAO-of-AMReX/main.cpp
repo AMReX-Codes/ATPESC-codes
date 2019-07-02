c350421c1bc40316f5bac49b51e88dd264747b97
Donald E. Willcox <dewillcox@lbl.gov>
2019-07-01 18:15:27 -0700

    WIP TAO/AMReX boundary control example.

---
+++ TAO-of-AMReX/main.cpp
@@ -0,0 +1,26 @@
+
+#include <AMReX.H>
+#include "MyTest.H"
+
+int main (int argc, char* argv[])
+{
+    amrex::Initialize(argc, argv);
+
+    {
+        BL_PROFILE("main");
+        MyTest mytest;
+
+        {
+            // get boundary values to set
+            mytest.update_boundary_values(); // set the boundary values
+            mytest.solve(); // solve the poisson problem with the boundary values
+            mytest.setup_adjoint_system(); // calculate the adjoint system rhs
+            mytest.solve_adjoint_system(); // solve for the adjoint
+            mytest.calculate_opt_gradient(); // calculate the adjoint contribution to df/dp
+        }
+        
+        mytest.writePlotfile();
+    }
+
+    amrex::Finalize();
+}
