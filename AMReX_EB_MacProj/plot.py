c6b0541ade75b48e6a9848ae1348c86a1c14cb80
Donald E. Willcox <dewillcox@lbl.gov>
2019-07-10 14:17:16 -0700

    plot.py takes a field

--- AMReX_EB_MacProj/plot.py
+++ AMReX_EB_MacProj/plot.py
@@ -5,10 +5,11 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('infile', type=str, help='Name of input file to plot.')
+parser.add_argument('-f', '--field', type=str, default='xvel', help='Name of field to plot.')
 args = parser.parse_args()
 
 if __name__ == "__main__":
     ds = AMReXDataset(args.infile)
-    s = yt.SlicePlot(ds, 'z', 'xvel')
+    s = yt.SlicePlot(ds, 'z', args.field)
     s.annotate_particles(1.0)
     s.save("{}.png".format(args.infile))
