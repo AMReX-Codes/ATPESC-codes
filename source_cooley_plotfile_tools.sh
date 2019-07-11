0000b8422cd1f2b92caa317a8d19cc6ca030a1ec
Donald E. Willcox <eugene.willcox@gmail.com>
2019-07-10 22:49:50 -0700

    Added a script to `source` to add AMReX plotfile tools to your PATH on Cooley.

---
+++ source_cooley_plotfile_tools.sh
@@ -0,0 +1,3 @@
+# `source` this file to add the directory for AMReX plotfile tools to your PATH on Cooley:
+# e.g. do `source source_cooley_plotfile_tools.sh`
+export PATH="/projects/ATPESC2019/FASTMath/spack/opt/spack/linux-rhel7-x86_64/gcc-4.8.5/amrex-develop-fvkknjb363aqxpt6zu2tt6oojixnlnex/Tools/Plotfile:$PATH"
