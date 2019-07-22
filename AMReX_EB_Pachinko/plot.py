3ec61a1ccb1e9e2dc3241dae166fdc23887d09c3
Ann Almgren <asalmgren@lbl.gov>
2019-07-21 19:13:39 -0700

    Make this read the proc rather than vel variable

--- AMReX_EB_Pachinko/plot.py
+++ AMReX_EB_Pachinko/plot.py
@@ -9,6 +9,6 @@ args = parser.parse_args()
 
 if __name__ == "__main__":
     ds = AMReXDataset(args.infile)
-    s = yt.SlicePlot(ds, 'z', 'after-vx')
+    s = yt.SlicePlot(ds, 'z', 'proc', origin="native")
     s.annotate_particles(1.0)
     s.save("{}.png".format(args.infile))
