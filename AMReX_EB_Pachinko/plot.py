1e6947203ac510b41bdead7cbc9d54ad00033236
Donald E. Willcox <eugene.willcox@gmail.com>
2019-07-27 22:49:20 -0700

    Move shell loop into the python for the pachinko vis too.

--- AMReX_EB_Pachinko/plot.py
+++ AMReX_EB_Pachinko/plot.py
@@ -2,14 +2,31 @@
 import yt
 from yt.frontends.boxlib.data_structures import AMReXDataset
 import argparse
+import re
+import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('infile', type=str, help='Name of input file to plot.')
+
+parser.add_argument('-i', '--infiles', type=str, default="\Aplt[0-9]*\Z", help='Regex for input files to plot.')
+parser.add_argument('-f', '--field', type=str, default='vfrac', help='Name of field to plot.')
 args = parser.parse_args()
 
-if __name__ == "__main__":
-    ds = AMReXDataset(args.infile)
+def doit(filename):
+    ds = AMReXDataset(filename)
     s = yt.SlicePlot(ds, 'z', 'vfrac',origin="native")
     s.set_log("vfrac", 0)
     s.annotate_particles(1.0, p_size = 500.0)
-    s.save("{}.png".format(args.infile))
+    s.save("{}.png".format(filename))
+
+if __name__ == "__main__":
+    regexp = re.compile(args.infiles)
+    # Files in the current directory
+    cwd_files = os.listdir()
+    # Filter files by regexp and skip if {file}.skip exists
+    subject_files = []
+    for f in cwd_files:
+        if regexp.match(f):
+            subject_files.append(f)
+
+    for f in subject_files:
+        doit(f)
