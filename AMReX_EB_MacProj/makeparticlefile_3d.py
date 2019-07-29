7c2076a5d75bf9c95cc0613141012b44b5c6a813
Ann Almgren <asalmgren@lbl.gov>
2019-07-29 10:36:19 -0700

    Make the 3d version so we can use paraview

--- AMReX_EB_MacProj/makeparticlefile_3d.py
+++ AMReX_EB_MacProj/makeparticlefile_3d.py
@@ -5,27 +5,25 @@
 
 import numpy as np
 
-NX = 3
-NY = 5
-NZ = 5
+NX = 5
+NY = 50
 
 dimx = [0,0.1]  #They'll start evenly close to the edge/source and flow through
 dimy = [0,1]
-dimz = [0,1]
 
-dx = (dimx[1] - dimx[0]) / NX
-dy = (dimy[1] - dimy[0]) / NY
-dz = (dimz[1] - dimz[0]) / NZ
+dx = (dimx[1] - dimx[0]) / NX 
+dy = (dimy[1] - dimy[0]) / NY 
 
-f = open("tracers_file", "w+")
+f = open("tracers_file_3d", "w")
+
+z_loc = 0.5
 
 for i in range(NX):
     for j in range(NY):
-        for k in range(NZ):
-            if (i==0 and j==0 and k==0):
-                f.write("%d \r\n" % (NX*NY*NZ))
-                f.write("%f %f %f \r\n" % ((i+1)*dx, (j+1)*dy, (k+1)*dz))
-            else:
-                f.write("%f %f %f \r\n" % ((i+1)*dx, (j+1)*dy, (k+1)*dz))
+        if (i==0 and j==0):
+            f.write("%d \r\n" % (NX*NY))
+            f.write("%f %f %f \r\n" % ((i+1)*dx, (j+1)*dy, z_loc))
+        else:
+            f.write("%f %f %f \r\n" % ((i+1)*dx, (j+1)*dy, z_loc))
 f.close()
                 
