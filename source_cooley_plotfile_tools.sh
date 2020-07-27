3a7ed77a04f818e7cb2902a8101b241fddef5587
Daniel R. Reynolds <reynolds@smu.edu>
2020-07-27 14:27:37 +0000

    Updates to source_cooley_plotfile_tools.sh for ATPESC 2020

--- source_cooley_plotfile_tools.sh
+++ source_cooley_plotfile_tools.sh
@@ -1,3 +1,4 @@
 # `source` this file to add the directory for AMReX plotfile tools to your PATH on Cooley:
 # e.g. do `source source_cooley_plotfile_tools.sh`
-export PATH="/projects/ATPESC2019/FASTMath/spack/opt/spack/linux-rhel7-x86_64/gcc-4.8.5/amrex-develop-fvkknjb363aqxpt6zu2tt6oojixnlnex/Tools/Plotfile:$PATH"
+#export PATH="/projects/ATPESC2019/FASTMath/spack/opt/spack/linux-rhel7-x86_64/gcc-4.8.5/amrex-develop-fvkknjb363aqxpt6zu2tt6oojixnlnex/Tools/Plotfile:$PATH"
+export PATH="/lus/theta-fs0/projects/ATPESC2020/MathPackagesTraining2020/spack/opt/spack/linux-rhel7-haswell/gcc-8.2.0/amrex-19.08-s7qw57lejtso4gglyl7wdezjq5qyzbdm/Tools/Plotfile:$PATH"
