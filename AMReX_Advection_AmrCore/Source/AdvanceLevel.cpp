6fb51c2007e852e77b5c9af0dc370b8f8a06c602
Donald E. Willcox <eugene.willcox@gmail.com>
2020-07-29 10:46:49 -0700

    print dt_lev instead of center time on CFL violation

--- AMReX_Advection_AmrCore/Source/AdvanceLevel.cpp
+++ AMReX_Advection_AmrCore/Source/AdvanceLevel.cpp
@@ -417,10 +417,10 @@ AmrCoreAdv::Advance (int lev, Real time, Real dt_lev, int iteration, int ncycle)
     {
 #if (AMREX_SPACEDIM > 2)
         amrex::Print() << "umax = " << umax << ", vmax = " << vmax << ", wmax = " << wmax 
-                       << ", dt = " << ctr_time << " dx = " << dx[1] << " " << dx[2] << " " << dx[3] << std::endl;
+                       << ", dt = " << dt_lev << " dx = " << dx[1] << " " << dx[2] << " " << dx[3] << std::endl;
 #else
         amrex::Print() << "umax = " << umax << ", vmax = " << vmax 
-                       << ", dt = " << ctr_time << " dx = " << dx[1] << " " << dx[2] << " " << dx[3] << std::endl;
+                       << ", dt = " << dt_lev << " dx = " << dx[1] << " " << dx[2] << " " << dx[3] << std::endl;
 #endif
         amrex::Abort("CFL violation. use smaller adv.cfl.");
     }
