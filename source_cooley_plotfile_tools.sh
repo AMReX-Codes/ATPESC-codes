de3264e88cd13ee9884ab5f106803f58dcd6c287
Daniel R. Reynolds <reynolds@smu.edu>
2021-07-30 19:59:10 +0000

    Updated paths for ATPESC 2021 shared install locations

--- source_cooley_plotfile_tools.sh
+++ source_cooley_plotfile_tools.sh
@@ -1,3 +1,3 @@
 # `source` this file to add the directory for AMReX plotfile tools to your PATH on Cooley:
 # e.g. do `source source_cooley_plotfile_tools.sh`
-export PATH="/gpfs/mira-home/balay/spack/opt/spack/linux-rhel7-haswell/gcc-8.2.0/amrex-21.07-5zqi6hw5mzgyof6xddypdjj4tdxsbk75/bin:$PATH"
+export PATH="/lus/grand/projects/ATPESC2021/usr/MathPackages/spack/opt/spack/linux-rhel7-haswell/gcc-8.2.0/amrex-21.07-iwaygptbziyyoogyo55ktavrf7vrfoqa/bin:$PATH"
